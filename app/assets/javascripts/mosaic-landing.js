1d4138bccc98ca392286bbfe2c1d0591afa1ae1a
Martin Debrunne <martin.debrunne@gmail.com>
2016-03-03 15:16:13 +0100

    add div for contents

--- app/assets/javascripts/mosaic-landing.js
+++ app/assets/javascripts/mosaic-landing.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $('.mosaic .mouse-hover').hover(function() {
-    $('.mosaic .block-fullscreen').html($(this).parent().parent().html());
-    $('.mosaic .block-fullscreen').find('.mouse-hover').remove();
+    $('.mosaic .block-fullscreen').html($(this).parent().parent().parent().html());
+    $('.mosaic .block-fullscreen').find('.content').remove();
     $('.mosaic .block').css('opacity', '0');
-    $(this).parent().parent().css('opacity', '1');
+    $(this).parent().parent().parent().css('opacity', '1');
   }, function() {
     $('.mosaic .block').css('opacity', '1');
   });
